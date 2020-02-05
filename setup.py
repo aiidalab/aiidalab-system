46324f153ed342041825635e06e9ef6a32df5dbe
Carl Simon Adorf <simon.adorf@epfl.ch>
2020-02-05 17:17:19 +0100

    Rename package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='AiiDA-lab core',
+    name='AiiDA lab system',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
